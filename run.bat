25690359302d514ad029bbaa20549bff1f2c172f
soaringsoul <951683309@qq.com>
2018-01-22 14:01:41 +0800

    run.py修改

--- run.bat
+++
@@ -1,2 +0,0 @@
-workon api_manager_venv
-fabmanager run
--- run.py
+++ run.py
@@ -1,4 +1,6 @@
 from app import app
 
-app.run(host='0.0.0.0', port=8080, debug=True)
+if __name__ == "__main__":
+
+	app.run(host='0.0.0.0', port=8080, debug=True)
 
